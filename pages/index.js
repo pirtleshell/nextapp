1dbbdffa60574c8893a51b0f89d74a2cd36654a3
Robert Pirtle <astropirtle@gmail.com>
2020-03-13 14:23:19 -0700

    use dynamic routing and content creation

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,26 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
+
+const titles = [
+  'I am a post',
+  'I am a different post',
+  'And look! Another!',
+];
+
 export default function Index() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>A list of posts!</h1>
       <ul>
-        <li>
-          <Link href='/about'>
-            <a title='About'>About</a>
-          </Link>
-        </li>
+        { titles.map((title, i) => (<PostLink id={title} key={i} />)) }
       </ul>
     </Layout>
   );
---
+++ pages/p/[id].js
@@ -0,0 +1,17 @@
+
+import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
+
+const Page = () => {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <h1>{router.query.id}</h1>
+      <p>This is the blog post content.</p>
+      <p>testing!</p>
+    </Layout>
+  );
+};
+
+export default Page;
